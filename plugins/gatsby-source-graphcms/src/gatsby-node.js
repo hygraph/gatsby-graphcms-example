e8fd6497659bfa43a801eb10d0ee4cb33ad686cc
Ricky de Laveaga <git@rdela.com>
2018-03-08 15:37:36 -0800

    1.0.0 alpha testing v1

---
+++ plugins/gatsby-source-graphcms/src/gatsby-node.js
@@ -0,0 +1,40 @@
+import {GraphQLClient} from 'graphql-request';
+import {forEachObjIndexed} from 'ramda';
+import {createNodes} from './util';
+import {DEBUG_MODE} from './constants';
+import {
+  keywordsError,
+  checkForFaultyFields
+} from './faulty-keywords';
+
+exports.sourceNodes = async (
+  {boundActionCreators, reporter},
+  {endpoint, token, query}
+) => {
+  if (query) {
+    const {createNode} = boundActionCreators;
+
+    const clientOptions = {
+      headers: {
+        Authorization: token ? `Bearer ${token}` : undefined
+      }
+    };
+
+    const client = new GraphQLClient(endpoint, clientOptions);
+
+    const userQueryResult = await client.request(query);
+
+    // Keywords workaround
+    if (checkForFaultyFields(userQueryResult)) {
+      reporter.panic(`gatsby-source-graphcms: ${keywordsError}`);
+    }
+
+    if (DEBUG_MODE) {
+      const jsonUserQueryResult = JSON.stringify(userQueryResult, undefined, 2);
+      console.log(`\ngatsby-source-graphcms: GraphQL query results: ${jsonUserQueryResult}`);
+    }
+    forEachObjIndexed(createNodes(createNode, reporter), userQueryResult);
+  } else {
+    reporter.panic(`gatsby-source-graphcms: you need to provide a GraphQL query in the plugin 'query' parameter`);
+  }
+};
